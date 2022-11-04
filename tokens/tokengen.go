e47d9cfb8c829912f283204595d9a0bcf2441489
DieFleitas <diegofleitas98@outlook.es>
2022-11-04 14:34:43 -0300

    build validatetoken and updatealltoken functions

--- tokens/tokengen.go
+++ tokens/tokengen.go
@@ -7,7 +7,11 @@ import (
 
 	"github.com/DieFleitas/ecommerce-akhil/database"
 	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/net/context"
 )
 
 type SignedDetails struct{
@@ -54,10 +58,53 @@ func TokenGenerator(email, firstname, lastname, uid string) (signedtoken, signed
 
 }
 
-func ValidateToken(){
+func ValidateToken(signedtoken string) (claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(signedtoken, &SignedDetails{}, func(token *jwt.Token)(interface{}, error){
+		return []byte(SECRET_KEY), nil
+	})
 
+	if err != nil {
+		msg = err.Error()
+		return
+	}
+
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
+		msg = "the token is invalid"
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix(){
+		msg = "token is already expired"
+		return
+	}
+
+	return claims, msg
 }
 
-func UpdateAllTokens(){
+func UpdateAllTokens(signedtoken string, signedrefreshtoken string, userid string){
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+	var updateobj primitive.D
+
+	updateobj = append(updateobj, bson.E{Key:"token", Value: signedtoken})
+	updateobj = append(updateobj, bson.E{Key:"refreshtoken", Value: signedrefreshtoken})
+	updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateobj = append(updateobj, bson.E{Key:"updatedat", Value: updated_at})
+
+	upsert := true
+
+	filter := bson.M{"user_id": userid}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+	_, err := UserData.UpdateOne(ctx, filter, bson.D{
+		{Key:"$set", Value:updateobj},
+	}, &opt)
+	defer cancel()
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
 }
